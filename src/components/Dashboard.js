1755c595d1f3967ad97f4c81940ede7e87ee1b92
IvanWiesner <ivanwiesner@gmail.com>
2021-11-02 14:09:07 -0600

    login page cont

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -9,7 +9,32 @@ function Dashboard() {
     const [loggedIn, setLoggedIn] = useState('')
     const [errors, setErrors] = useState ('')
     const history = useHistory()
-    
+
+    function onSubmit(e){
+        e.preventDefault()
+        const login = {
+            email: email,
+            password
+        }
+        // let API_PATH 
+        // loggedIn?API_PATH = 'session' : API_PATH = "users"
+        fetch(`/login`, {
+            method:'POST',
+            headers:{'Content-Type': 'application/json'},
+            body:JSON.stringify(login)
+        })
+        .then(res => res.json())
+        .then(json => {
+            console.log(json)
+            if(json.error){
+                setErrors(json.error)
+            }else {
+                setLogin(true)
+                console.log(setLogin)
+                history.push("/")
+            }
+        })
+    }
   return (
     <div className="Dashboard">
       <p>dashboard</p>
