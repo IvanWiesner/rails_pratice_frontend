68b5f6bcf830c69a2dc4f35dd40103a2d1a0957d
IvanWiesner <ivanwiesner@gmail.com>
2021-11-02 13:34:26 -0600

    login

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import { useState } from "react";
+import { useHistory } from "react-router-dom"
 
 function Dashboard() {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [loggedIn, setLoggedIn] = useState('')
+    const [errors, setErrors] = useState ('')
+    const history = useHistory()
+    
   return (
     <div className="Dashboard">
       <p>dashboard</p>
